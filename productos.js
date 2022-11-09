7f638d01ab64e2bffb415aa882ac7881b5134b4b
zaba7 <tomizaba99@gmail.com>
2022-11-09 15:28:54 -0300

    primer commit

---
+++ productos.js
@@ -0,0 +1,62 @@
+const productos=[
+    {
+        id:1,
+        nombre:"Bacon King Carne",
+        precio: 1500,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/Bacon-King-Carne.png",    
+    },
+    {
+        id:2,
+        nombre:"Doble Cuarto XL",
+        precio: 1500,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_DOBLE-CUARTO-XL_1200X800_020822.png",    
+    },
+    {
+        id:3,
+        nombre:"Whopper de Guacamole",
+        precio: 1400,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_Whopper-Guacamole_1200X900_210922.png",    
+    },
+    {
+        id:4,
+        nombre:"Doble Napolitana",
+        precio: 1600,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/DobleNapolitano-XL-1.png",    
+    },
+    {
+        id:5,
+        nombre:"Stacker Triple",
+        precio: 1700,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_STACKER-XL-_1200X800_020822.png",    
+    },
+    {
+        id:6,
+        nombre:"Stacker Cuadruple",
+        precio: 1800,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/Stacker-Cuadruple.png",    
+    },
+    {
+        id:7,
+        nombre:"Nuggets",
+        precio: 900,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-bk-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BK_WEB_SEPTIEMBRE_MENU_NUGGETS-PLANT-BASE_270922_1200x800_.png",    
+    },
+    {
+        id:8,
+        nombre:"Balde de Papas",
+        precio: 1200,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/BaldePapas.png",
+    },{
+        id:9,
+        nombre:"Aros de cebolla",
+        precio: 900,
+        img: "https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/wordpress/web/app/uploads/sites/5/2021/02/aros-de-cebolla-mediano.png",
+    },
+    {
+        id:10,
+        nombre:"Papas Cheddar Bacon",
+        precio: 1000,
+        img:"https://s3-eu-central-1.amazonaws.com/www.burgerking.com.ar.v2/wp-media-folder-burger-king-argentina//home/ubuntu/preview/menu-app/frontend/apps/marketing-website-wordpress-app/web/app/uploads/sites/5/Papas-Cheddar-y-Bacon.png",
+    },
+
+];
